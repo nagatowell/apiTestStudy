eb32be7e42df49405ce8feb66966815bd6f1e8de
Wellington Rodrigues <wellington.rodrigues@psychemedics.com.br>
2017-12-02 00:45:35 -0200

    Atualizacao do db

--- config/database.php
+++ config/database.php
@@ -58,6 +58,7 @@ return [
             'driver'   => 'mongodb',
             'host'     => env('DB_HOST_MONGO', 'localhost'),
             'port'     => env('DB_PORT_MONGO', 27017),
+            'dsn'      => env('DB_DSN'),
             'database' => env('DB_DATABASE_MONGO'),
             'username' => env('DB_USERNAME_MONGO'),
             'password' => env('DB_PASSWORD_MONGO'),
