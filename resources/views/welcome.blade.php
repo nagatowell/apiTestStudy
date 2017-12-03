f828988cad7220a9919567b6f39a2fa2c8b59545
Wellington Rodrigues <wellington.rodrigues@psychemedics.com.br>
2017-12-03 15:38:32 -0200

    Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Nagato
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">Gente Fina</a>
+                    <a href="#">Gato</a>
+                    <a href="#">Gostoso</a>
+                    <a href="#">Garoto</a>
+                    <a href="www.github.com/nagatowell">GitHub</a>
                 </div>
             </div>
         </div>
